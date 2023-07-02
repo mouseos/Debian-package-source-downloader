dc6b9b31e3090ddec99bbef25a292d10f8747631
Syuugo <pub@s1204.me>
2023-07-02 10:52:36 +0900

    Fix `exit`

--- downloader.sh
+++ downloader.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-ex1() {
+abort() {
   echo -e "$1"
-  exit 1
+  if [ "$2" ]; then
+    exit "$2"
+  fi
 }
 
-whiptail --title "確認" --yesno "$(dpkg -l | grep -c '^ii' | cut -d " " -f1)個のパッケージを検出しました｡\nソースファイルを取得しますか？" 0 0 || ex1 "処理を中止しました｡"
+whiptail --title "確認" --yesno "$(dpkg -l | grep -c '^ii' | cut -d " " -f1)個のパッケージを検出しました｡\nソースファイルを取得しますか？" 0 0 || abort "処理を中止しました｡" 1
 
 clear
 
@@ -16,9 +18,8 @@ count=0
 while read -r line; do
   count=$((++count))
   echo -e "\n\n${count}個目：$line\n"
-  apt-get source "$line" || ex1 "\n例外なエラーが発生しました：$?"
+  apt-get source "$line" || abort "\n例外なエラーが発生しました：$?"
 done < ./sources.list.txt
 
 clear
-whiptail --msgbox "\nダウンロードが完了しました｡\n" 0 0
-exit 0
+abort "\nダウンロードが完了しました｡\n" 0
