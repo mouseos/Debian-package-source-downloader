873462daa704b62776bed7eebe117aee239e2383
Syuugo <pub@s1204.me>
2023-07-02 10:33:53 +0900

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if ! (whiptail --title "確認" --yesno "$(dpkg -l | grep -c '^ii' | cut -d " " -f1)個のパッケージを検出しました｡\nソースファイルを取得しますか？" 0 0); then
-  echo "処理を中止しました｡"
+ex1() {
+  echo -e "$1"
   exit 1
-fi
+}
+
+whiptail --title "確認" --yesno "$(dpkg -l | grep -c '^ii' | cut -d " " -f1)個のパッケージを検出しました｡\nソースファイルを取得しますか？" 0 0) || ex1 "処理を中止しました｡"
 
 clear
 
-mkdir -p ./package_sources/
-#shellcheck disable=SC2164
-cd ./package_sources/
+mkdir -p ./package_sources/ && cd ./package_sources/
 dpkg -l | grep '^ii' | awk '{print $2"="$3 > "./sources.list.txt"}'
 
 count=0
 while read -r line; do
   count=$((++count))
   echo -e "\n\n${count}個目：$line\n"
-  apt-get source "$line" || echo -e "\n例外なエラーが発生しました：$?"
+  apt-get source "$line" || ex1 "\n例外なエラーが発生しました：$?"
 done < ./sources.list.txt
 
 clear
