8b4ab6a39d24978638ca08acd2064255c69ab7eb
Syuugo <pub@s1204.me>
2023-08-13 20:20:31 +0900

    Shaping

--- downloader.sh
+++ downloader.sh
@@ -2,9 +2,7 @@
 
 abort() {
   echo -e "$1"
-  if [ "$2" ]; then
-    exit "$2"
-  fi
+  exit "$2"
 }
 
 whiptail --title "確認" --yesno "$(dpkg -l | grep -c '^ii')個のパッケージを検出しました｡\nソースファイルを取得しますか？" 0 0 || abort "処理を中止しました｡" 1
@@ -18,8 +16,7 @@ count=0
 while read -r line; do
   count=$((++count))
   echo -e "\n\n${count}個目：$line\n"
-  apt-get source "$line" || abort "\n例外なエラーが発生しました：$?"
+  apt-get source "$line" || abort "\n例外なエラーが発生しました：$?" $?
 done < ./sources.list.txt
 
-clear
-abort "\nダウンロードが完了しました｡\n" 0
+abort "\n\nダウンロードが完了しました｡\n" 0
